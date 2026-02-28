e23d1cd0c8e31f9f7e228271015d78136b4e6af6
TerryForgetfull <160047138+toSTARdo@users.noreply.github.com>
2026-02-28 13:15:25 +0000

    fix: forgotten router

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,5 +1,7 @@
 from aiogram import Router
+from .onboarding import router as onboarding_router
 
 def get_handlers_router() -> Router:
     router = Router()
+    router.include_router(onboarding_router)
     return router
