d05cf73f966873cf6c56c88330b9983d23ad88c2
TerryForgetfull <160047138+toSTARdo@users.noreply.github.com>
2026-02-28 14:21:15 +0000

    fix: added router

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,7 +1,9 @@
 from aiogram import Router
 from .onboarding import router as onboarding_router
+from .handlers.profile.view import router as profile_router
 
 def get_handlers_router() -> Router:
     router = Router()
     router.include_router(onboarding_router)
+    router.include_router(profile_router)
     return router
